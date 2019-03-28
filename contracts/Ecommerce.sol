bb14dba1b21122240d57b9f08573755711cb4560
merlox <merunasgrincalaitis@gmail.com>
2019-03-28 23:15:27 +0100

    Working on the ERC721 implementation

---
+++ contracts/Ecommerce.sol
@@ -0,0 +1,139 @@
+pragma ^0.5.5;
+
+import './ERC721.sol';
+
+contract ERC721 is ERC165, IERC721 {
+    using SafeMath for uint256;
+    using Address for address;
+    using Counters for Counters.Counter;
+    bytes4 private constant _ERC721_RECEIVED = 0x150b7a02;
+    mapping (uint256 => address) private _tokenOwner;
+    mapping (uint256 => address) private _tokenApprovals;
+    mapping (address => Counters.Counter) private _ownedTokensCount;
+    mapping (address => mapping (address => bool)) private _operatorApprovals;
+    bytes4 private constant _INTERFACE_ID_ERC721 = 0x80ac58cd;
+    constructor () public {
+        _registerInterface(_INTERFACE_ID_ERC721);
+    }
+    function balanceOf(address owner) public view returns (uint256) {
+        require(owner != address(0));
+        return _ownedTokensCount[owner].current();
+    }
+    function ownerOf(uint256 tokenId) public view returns (address) {
+        address owner = _tokenOwner[tokenId];
+        require(owner != address(0));
+        return owner;
+    }
+    function approve(address to, uint256 tokenId) public {
+        address owner = ownerOf(tokenId);
+        require(to != owner);
+        require(msg.sender == owner || isApprovedForAll(owner, msg.sender));
+        _tokenApprovals[tokenId] = to;
+        emit Approval(owner, to, tokenId);
+    }
+    function getApproved(uint256 tokenId) public view returns (address) {
+        require(_exists(tokenId));
+        return _tokenApprovals[tokenId];
+    }
+    function setApprovalForAll(address to, bool approved) public {
+        require(to != msg.sender);
+        _operatorApprovals[msg.sender][to] = approved;
+        emit ApprovalForAll(msg.sender, to, approved);
+    }
+    function isApprovedForAll(address owner, address operator) public view returns (bool) {
+        return _operatorApprovals[owner][operator];
+    }
+    function transferFrom(address from, address to, uint256 tokenId) public {
+        require(_isApprovedOrOwner(msg.sender, tokenId));
+        _transferFrom(from, to, tokenId);
+    }
+    function safeTransferFrom(address from, address to, uint256 tokenId) public {
+        safeTransferFrom(from, to, tokenId, "");
+    }
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public {
+        transferFrom(from, to, tokenId);
+        require(_checkOnERC721Received(from, to, tokenId, _data));
+    }
+    function _exists(uint256 tokenId) internal view returns (bool) {
+        address owner = _tokenOwner[tokenId];
+        return owner != address(0);
+    }
+    function _isApprovedOrOwner(address spender, uint256 tokenId) internal view returns (bool) {
+        address owner = ownerOf(tokenId);
+        return (spender == owner || getApproved(tokenId) == spender || isApprovedForAll(owner, spender));
+    }
+    function _mint(address to, uint256 tokenId) internal {
+        require(to != address(0));
+        require(!_exists(tokenId));
+        _tokenOwner[tokenId] = to;
+        _ownedTokensCount[to].increment();
+        emit Transfer(address(0), to, tokenId);
+    }
+    function _burn(address owner, uint256 tokenId) internal {
+        require(ownerOf(tokenId) == owner);
+        _clearApproval(tokenId);
+        _ownedTokensCount[owner].decrement();
+        _tokenOwner[tokenId] = address(0);
+        emit Transfer(owner, address(0), tokenId);
+    }
+    function _burn(uint256 tokenId) internal {
+        _burn(ownerOf(tokenId), tokenId);
+    }
+    function _transferFrom(address from, address to, uint256 tokenId) internal {
+        require(ownerOf(tokenId) == from);
+        require(to != address(0));
+        _clearApproval(tokenId);
+        _ownedTokensCount[from].decrement();
+        _ownedTokensCount[to].increment();
+        _tokenOwner[tokenId] = to;
+        emit Transfer(from, to, tokenId);
+    }
+    function _checkOnERC721Received(address from, address to, uint256 tokenId, bytes memory _data)
+        internal returns (bool)
+    {
+        if (!to.isContract()) {
+            return true;
+        }
+        bytes4 retval = IERC721Receiver(to).onERC721Received(msg.sender, from, tokenId, _data);
+        return (retval == _ERC721_RECEIVED);
+    }
+    function _clearApproval(uint256 tokenId) private {
+        if (_tokenApprovals[tokenId] != address(0)) {
+            _tokenApprovals[tokenId] = address(0);
+        }
+    }
+}
+
+/// @notice The Ecommerce Token that implements the ERC721 token with mint functions
+/// @author Merunas Grincalaitis <merunasgrincalaitis@gmail.com>
+contract EcommerceToken is ERC721 {
+    address public owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, 'This function can only be called by the owner');
+        _;
+    }
+
+    /// @notice To generate a new token for the specified address, only executable by the contract owner
+    /// @param _to The receiver of this new token
+    /// @param _tokenId The new token id, must be unique
+    function mint(address _to, uint256 _tokenId) public onlyOwner {
+        _mint(_to, _tokenId);
+    }
+
+    /// @notice To delete an existing token if you're the owner of that token
+    /// @param _tokenId The id of the token you want to burn
+    function burn(uint256 _tokenId) public {
+        _burn(_tokenId);
+    }
+
+    /// @notice To setup a new owner
+    /// @param _newOwner The new owner
+    function transferOwnership(address _newOwner) public onlyOwner {
+        owner = _newOwner;
+    }
+}
+
+contract Ecommerce {
+
+}
