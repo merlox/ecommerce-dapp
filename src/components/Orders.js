37e8737acaefa53dc4eff0923ceb8c146a0a04d8
merlox <merunasgrincalaitis@gmail.com>
2019-04-02 16:27:56 +0200

    Working order loading with great design

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -7,49 +7,12 @@ class Orders extends Component {
 
         // We'll separate the completed vs the pending based on the order state
         this.state = {
-            sellOrders: [{
-                id: 1,
-                title: 'Clasic trendy shoes',
-                description: 'New unique shoes for sale',
-                date: Date.now(),
-                owner: '',
-                price: 12,
-                image: 'https://cdn.shopify.com/s/files/1/2494/8702/products/Bjakin-2018-Socks-Running-Shoes-for-Men-Lightweight-Sports-Sneakers-Colors-Man-Sock-Walking-Shoes-Big_17fa0d5b-d9d9-46a0-bdea-ac2dc17474ce_400x.jpg?v=1537755930',
-                purchasedAt: Date.now(),
-                state: 'completed',
-            }, {
-                id: 3,
-                title: 'White shoes unisex',
-                description: "100% Synthetic Imported \nRubber sole \nShaft measures approximately Low-Top from arch \nLace-up skate shoe with smooth abrasion-resistant upper featuring signature 3-Stripes logoing and wraparound midsole \nGrippy vulcanized rubber outsole sticks to board for control",
-                date: Date.now(),
-                owner: '',
-                price: 28,
-                image: 'http://www.cottageartcreations.com/wp-content/uploads/2017/09/white-shoes-aliexpress-com-buy-new-men-flat-shoes-spring-autumn-black-white-man-srjqhnn-.jpg',
-                state: 'pending',
-            }],
-            buyOrders: [{
-                id: 2,
-                title: 'Flat heel shoes',
-                description: 'For women yellow spring shoes',
-                date: Date.now(),
-                owner: '',
-                price: 62,
-                image: 'https://ae01.alicdn.com/kf/HTB10VmYPFXXXXckXFXXq6xXFXXXY/Spring-and-Autumn-Flats-Women-Flat-heel-Shoes-Fashion-Leopard-Flats-Women-Shoes-Casual-Soft-Comfortable.jpg_640x640.jpg',
-                state: 'pending',
-            }, {
-                id: 3,
-                title: 'White shoes unisex',
-                description: "100% Synthetic Imported \nRubber sole \nShaft measures approximately Low-Top from arch \nLace-up skate shoe with smooth abrasion-resistant upper featuring signature 3-Stripes logoing and wraparound midsole \nGrippy vulcanized rubber outsole sticks to board for control",
-                date: Date.now(),
-                owner: '',
-                price: 28,
-                image: 'http://www.cottageartcreations.com/wp-content/uploads/2017/09/white-shoes-aliexpress-com-buy-new-men-flat-shoes-spring-autumn-black-white-man-srjqhnn-.jpg',
-                state: 'completed',
-            }],
-            pendingSellOrdersHtml: [],
-            pendingBuyOrdersHtml: [],
-            completedSellOrdersHtml: [],
-            completedBuyOrdersHtml: [],
+            pendingSellerOrders: [],
+            pendingBuyerOrders: [],
+            completedOrders: [],
+            pendingSellerOrdersHtml: [],
+            pendingBuyerOrdersHtml: [],
+            completedOrdersHtml: [],
         }
         this.setup()
     }
@@ -78,89 +41,175 @@ class Orders extends Component {
 
         // In reverse to get the most recent orders first
         for(let i = pendingSellerOrdersLength; i > conditionSeller; i--) {
-            const order = await contract.methods.pendingSellerOrders(user, i - 1).call()
-            console.log('order', order)
+            let order = await contract.methods.pendingSellerOrders(user, i - 1).call()
+            pendingSellerOrders.push(await this.generateOrderObject(order))
         }
 
         for(let i = pendingBuyerOrdersLength; i > conditionBuyer; i--) {
-            const order = await contract.methods.pendingBuyerOrders(user, i - 1).call()
-            console.log('order', order)
+            let order = await contract.methods.pendingBuyerOrders(user, i - 1).call()
+            pendingBuyerOrders.push(await this.generateOrderObject(order))
         }
 
         for(let i = completedOrdersLength; i > conditionCompleted; i--) {
-            const order = await contract.methods.completedOrders(user, i - 1).call()
-            console.log('order', order)
+            let order = await contract.methods.completedOrders(user, i - 1).call()
+            completedOrders.push(await this.generateOrderObject(order))
         }
-        // Get the orders and display the shipping address in a box
-        // TODO Show the addresses and completed orders in one box instead of 2
+        this.setState({pendingSellerOrders, pendingBuyerOrders, completedOrders})
+    }
+
+    async generateOrderObject(order) {
+        let productAssociated = await contract.methods.productById(parseInt(order.id)).call()
+        order = {
+            id: parseInt(order.id),
+            buyer: order.buyer,
+            nameSurname: order.nameSurname,
+            lineOneDirection: order.lineOneDirection,
+            lineTwoDirection: order.lineTwoDirection,
+            city: myWeb3.utils.toUtf8(order.city),
+            stateRegion: myWeb3.utils.toUtf8(order.stateRegion),
+            postalCode: String(order.postalCode),
+            country: myWeb3.utils.toUtf8(order.country),
+            phone: String(order.phone),
+            state: order.state,
+            date: String(productAssociated.date),
+            description: productAssociated.description,
+            image: productAssociated.image,
+            owner: productAssociated.owner,
+            price: myWeb3.utils.fromWei(String(productAssociated.price)),
+            title: productAssociated.title,
+        }
+        return order
     }
 
     async markAsCompleted(product) {}
 
     async displayOrders() {
-        let pendingSellOrdersHtml = []
-        let pendingBuyOrdersHtml = []
-        let completedSellOrdersHtml = []
-        let completedBuyOrdersHtml = []
-        await this.state.sellOrders.asyncForEach(product => {
-            if(product.state == 'pending') {
-                pendingSellOrdersHtml.push(
-                    <div key={product.id} className="product">
-                        <img className="product-image" src={product.image} />
-                        <div className="product-data">
-                            <h3 className="small-product-title">{product.title}</h3>
-                            <div className="product-state">State: {product.state}</div>
-                            <div className="product-description">{product.description.substring(0, 15) + '...'}</div>
-                            <div className="product-price">{product.price} ETH</div>
-                            <button className="small-view-button" onClick={() => {
-                                this.props.setState({product})
-                                this.props.redirectTo('/product')
-                            }} type="button">View</button>
-                            <button className="small-completed-button" onClick={() => {
-                                this.markAsCompleted(product)
-                            }} type="button">Mark as completed</button>
-                        </div>
-                    </div>
-                )
-            } else {
-                completedSellOrdersHtml.push(
-                    <div key={product.id} className="product">
-                        <img className="product-image" src={product.image} />
-                        <div className="product-data">
-                            <h3 className="product-title">{product.title}</h3>
-                            <div className="product-state">State: {product.state}</div>
-                            <div className="product-description">{product.description.substring(0, 15) + '...'}</div>
-                            <div className="product-price">{product.price} ETH</div>
-                            <button onClick={() => {
-                                this.props.setState({product})
-                                this.props.redirectTo('/product')
-                            }} className="product-view" type="button">View</button>
-                        </div>
-                    </div>
-                )
-            }
-        })
-        await this.state.buyOrders.asyncForEach(product => {
-            let html = (
-                <div key={product.id} className="product">
-                    <img className="product-image" src={product.image} />
+        let pendingSellerOrdersHtml = []
+        let pendingBuyerOrdersHtml = []
+        let completedOrdersHtml = []
+
+        await this.state.pendingSellerOrders.asyncForEach(order => {
+            pendingSellerOrdersHtml.push(
+                <div key={order.id} className="product">
+                    <img className="product-image" src={order.image} />
                     <div className="product-data">
-                        <h3 className="product-title">{product.title}</h3>
-                        <div className="product-state">State: {product.state}</div>
-                        <div className="product-description">{product.description.substring(0, 15) + '...'}</div>
-                        <div className="product-price">{product.price} ETH</div>
-                        <button onClick={() => {
-                            this.props.setState({product})
+                        <h3 className="small-product-title">{order.title}</h3>
+                        <div className="product-state">State: {order.state}</div>
+                        <div className="product-description">{order.description.substring(0, 15) + '...'}</div>
+                        <div className="product-price">{order.price} ETH</div>
+                        <button className="small-view-button" onClick={() => {
+                            this.props.setState({product: order})
                             this.props.redirectTo('/product')
-                        }} className="product-view" type="button">View</button>
+                        }} type="button">View</button>
+                        <button className="small-completed-button" onClick={() => {
+                            this.markAsCompleted(product)
+                        }} type="button">Mark as completed</button>
+                    </div>
+                    <div className="order-address">
+                        <div>Buyer</div>
+                        <div className="second-column" title={order.buyer}>{order.buyer}</div>
+                        <div>Name and surname</div>
+                        <div className="second-column" title={order.nameSurname}>{order.nameSurname}</div>
+                        <div>Line 1 direction</div>
+                        <div className="second-column" title={order.lineOneDirection}>{order.lineOneDirection}</div>
+                        <div>Line 2 direction</div>
+                        <div className="second-column" title={order.lineTwoDirection}>{order.lineTwoDirection}</div>
+                        <div>City</div>
+                        <div className="second-column" title={order.city}>{order.city}</div>
+                        <div>State or region</div>
+                        <div className="second-column" title={order.stateRegion}>{order.stateRegion}</div>
+                        <div>Postal code</div>
+                        <div className="second-column">{order.postalCode}</div>
+                        <div>Country</div>
+                        <div className="second-column" title={order.country}>{order.country}</div>
+                        <div>Phone</div>
+                        <div className="second-column">{order.phone}</div>
+                        <div>State</div>
+                        <div className="second-column" title={order.state}>{order.state}</div>
                     </div>
                 </div>
             )
-
-            if(product.state == 'pending') pendingBuyOrdersHtml.push(html)
-            else completedBuyOrdersHtml.push(html)
         })
-        this.setState({pendingSellOrdersHtml, pendingBuyOrdersHtml, completedSellOrdersHtml, completedBuyOrdersHtml})
+        await this.state.pendingBuyerOrders.asyncForEach(order => {
+            pendingBuyerOrdersHtml.push(
+                <div key={order.id} className="product">
+                    <img className="product-image" src={order.image} />
+                    <div className="product-data">
+                        <h3 className="product-title">{order.title}</h3>
+                        <div className="product-state">State: {order.state}</div>
+                        <div className="product-description">{order.description.substring(0, 15) + '...'}</div>
+                        <div className="product-price">{order.price} ETH</div>
+                        <button onClick={() => {
+                            this.props.setState({product: order})
+                            this.props.redirectTo('/product')
+                        }} className="product-view" type="button">View</button>
+                    </div>
+                    <div className="order-address">
+                        <div>Buyer</div>
+                        <div className="second-column" title={order.buyer}>{order.buyer}</div>
+                        <div>Name and surname</div>
+                        <div className="second-column" title={order.nameSurname}>{order.nameSurname}</div>
+                        <div>Line 1 direction</div>
+                        <div className="second-column" title={order.lineOneDirection}>{order.lineOneDirection}</div>
+                        <div>Line 2 direction</div>
+                        <div className="second-column" title={order.lineTwoDirection}>{order.lineTwoDirection}</div>
+                        <div>City</div>
+                        <div className="second-column" title={order.city}>{order.city}</div>
+                        <div>State or region</div>
+                        <div className="second-column" title={order.stateRegion}>{order.stateRegion}</div>
+                        <div>Postal code</div>
+                        <div className="second-column">{order.postalCode}</div>
+                        <div>Country</div>
+                        <div className="second-column" title={order.country}>{order.country}</div>
+                        <div>Phone</div>
+                        <div className="second-column">{order.phone}</div>
+                        <div>State</div>
+                        <div className="second-column" title={order.state}>{order.state}</div>
+                    </div>
+                </div>
+            )
+        })
+        await this.state.completedOrders.asyncForEach(order => {
+            completedOrdersHtml.push(
+                <div key={order.id} className="product">
+                    <img className="product-image" src={order.image} />
+                    <div className="product-data">
+                        <h3 className="product-title">{order.title}</h3>
+                        <div className="product-state">State: {order.state}</div>
+                        <div className="product-description">{order.description.substring(0, 15) + '...'}</div>
+                        <div className="product-price">{order.price} ETH</div>
+                        <button onClick={() => {
+                            this.props.setState({product: order})
+                            this.props.redirectTo('/product')
+                        }} className="product-view" type="button">View</button>
+                    </div>
+                    <div className="order-address">
+                        <div>Buyer</div>
+                        <div className="second-column" title={order.buyer}>{order.buyer}</div>
+                        <div>Name and surname</div>
+                        <div className="second-column" title={order.nameSurname}>{order.nameSurname}</div>
+                        <div>Line 1 direction</div>
+                        <div className="second-column" title={order.lineOneDirection}>{order.lineOneDirection}</div>
+                        <div>Line 2 direction</div>
+                        <div className="second-column" title={order.lineTwoDirection}>{order.lineTwoDirection}</div>
+                        <div>City</div>
+                        <div className="second-column" title={order.city}>{order.city}</div>
+                        <div>State or region</div>
+                        <div className="second-column" title={order.stateRegion}>{order.stateRegion}</div>
+                        <div>Postal code</div>
+                        <div className="second-column">{order.postalCode}</div>
+                        <div>Country</div>
+                        <div className="second-column" title={order.country}>{order.country}</div>
+                        <div>Phone</div>
+                        <div className="second-column">{order.phone}</div>
+                        <div>State</div>
+                        <div className="second-column" title={order.state}>{order.state}</div>
+                    </div>
+                </div>
+            )
+        })
+
+        this.setState({pendingSellerOrdersHtml, pendingBuyerOrdersHtml, completedOrdersHtml})
     }
 
     render() {
@@ -170,22 +219,17 @@ class Orders extends Component {
                 <div className="orders-page">
                     <div>
                         <h3 className="order-title">PENDING ORDERS AS A SELLER</h3>
-                        {this.state.pendingSellOrdersHtml}
+                        {this.state.pendingSellerOrdersHtml}
                     </div>
 
                     <div>
                         <h3 className="order-title">PENDING ORDERS AS A BUYER</h3>
-                        {this.state.pendingBuyOrdersHtml}
+                        {this.state.pendingBuyerOrdersHtml}
                     </div>
 
-                    <div>
-                        <h3 className="order-title">COMPLETED SELL ORDERS</h3>
-                        {this.state.completedSellOrdersHtml}
-                    </div>
-
-                    <div>
-                        <h3 className="order-title">COMPLETED BUY ORDERS</h3>
-                        {this.state.completedBuyOrdersHtml}
+                    <div className="completed-orders-container">
+                        <h3 className="order-title">COMPLETED ORDERS</h3>
+                        {this.state.completedOrdersHtml}
                     </div>
                 </div>
             </div>
